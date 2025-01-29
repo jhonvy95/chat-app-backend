14efd47b1cb943b705a92d8f09e8031472d3bc73
jhon <jyate@smartsoft.la>
2025-01-29 12:47:03 -0500

    Database Setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./lib/db.js";
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+dotenv.config();
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log("Server running on port 5001");
+app.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
 });
