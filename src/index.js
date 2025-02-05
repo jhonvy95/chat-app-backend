a49ec7600641654b7e986ee3c75329f7b0a9447a
jhon <jyate@smartsoft.la>
2025-02-04 23:04:06 -0500

    Signup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
+import express from "express";
 import { connectDB } from "./lib/db.js";
-const app = express();
-const PORT = process.env.PORT || 3000;
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server running on port: " + PORT);
+  console.log(`Server running on port: ${PORT}`);
   connectDB();
 });
